022ecfe534eaca3990f7d83f64309301501af3ac
oefinger <oefinger@gmail.com>
2015-06-14 17:46:47 -0500

    UI updates

--- www/js/ui.js
+++ www/js/ui.js
@@ -132,7 +132,7 @@ function moveTabMarker() {
 		{'left': '+=' + SCREEN_WIDTH},
 		SCROLLPERIOD, 'linear', iterateTabMarker);
 	*/
-	$('#tab_marker').css('transform','translateX(450px)');
+	$('#tab_marker').css('transform','translateX(450px)').css('transition-duration','3s');
 }
 
 var Note = function(string, fret, is_silent, timeInMs) {
