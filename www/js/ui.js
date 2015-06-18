8cf1199b298842ec1985b4399038f8e66330bd7c
oefinger <oefinger@gmail.com>
2015-06-18 10:21:46 -0500

    UI updates

--- www/js/ui.js
+++ www/js/ui.js
@@ -135,7 +135,9 @@ function moveTabMarker() {
 		SCROLLPERIOD, 'linear', iterateTabMarker);
 	*/
 	
-	$('#tab_marker').css('transform','translateX(' + SCREEN_WIDTH-(parseInt($('body').css('margin-left').replace('px',''))+parseInt($('body').css('margin-right').replace('px',''))) + 'px)').css('transition-duration',(SCROLLPERIOD/1000)'s').css('transition-timing-function','linear');
+	var tmp = (parseInt($('body').css('margin-left').replace('px',''))+parseInt($('body').css('margin-right').replace('px','')));
+	alert(tmp);
+	$('#tab_marker').css('transform','translateX(' + SCREEN_WIDTH + 'px)').css('transition-duration',(SCROLLPERIOD/1000)'s').css('transition-timing-function','linear');
 }
 
 var Note = function(string, fret, is_silent, timeInMs) {
