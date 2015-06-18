5a091c036d4e8015de0bd9cd371dbbf4f846e752
oefinger <oefinger@gmail.com>
2015-06-18 14:04:11 -0500

    UI updates

--- www/js/ui.js
+++ www/js/ui.js
@@ -128,6 +128,7 @@ function iterateTabMarker() {
 
 	SCROLL_INDEX = SCROLL_INDEX+1;
 	//$('.scrollstring').css('margin-left','-' + (SCREEN_WIDTH*SCROLL_INDEX-$('body').css('margin-left').replace('px','')) + 'px');
+	alert('iterate');
 	$('#tab_marker').css('transform','translateX(-' + ANIMATE_WIDTH + 'px)');
 	//moveTabMarker();
 }
